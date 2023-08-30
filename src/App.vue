a34ede0264d34b220fff43f005fc4d582f98abd5
hanifsyahsn <hanifsyah.nazir@styletheory.co>
2023-08-30 11:22:28 +0700

    FCM

---
+++ src/App.vue
@@ -0,0 +1,495 @@
+<script setup>
+document.title = 'H&I'
+</script>
+
+<template>
+  <header>
+  </header>
+  <head>
+    <link rel="icon" href="../public/favicon.ico">
+  </head>
+
+  <main>
+    <div id="test" class="container">
+        <div class="welcome_section_detail">
+          <div class="welcome_section_flower">&#10051;</div>
+          <div class="welcome_section_bismillah">&#65021;</div>
+          <div class="welcome_section_note">Save The Date</div>
+          <div class="welcome_section_cppcpw">Hanif & Intan</div>
+          <div class="welcome_section_date">23 September 2023</div>
+          <div class="welcome_section_love">&hearts;</div>
+      </div>
+      <div class="wedding_information">
+        <div class="wrapper">
+          <div class="wedding_information_flower">&#10048;</div>
+          <div class="wedding_information_salam">Assalamu'alaikum</div>
+          <div class="wedding_information_detail">Dengan Rahmat Allah yang Maha Kuasa InsyaAllah kami akan melangsungkan pernikahan pada 23 September 2023.</div>
+          <div class="wedding_information_cpp">
+            <p class="wedding_information_cpp_name">Hanifsyah Solehuddin Nazir, S.T.</p>
+            <p>Putra Dari</p>
+            <p style="width: 90%">Bapak (Alm.) Mohammad Sjarifuddin Nazir</p>
+            <p>Ibu Dra. Ardiana, M.Si.</p>
+          </div>
+          <div class="wedding_information_cpw">
+            <p class="wedding_information_cpw_name">Sri Intan, S.E.</p>
+            <p>Putri Dari</p>
+            <p>Bapak M. Rahmat</p>
+            <p>Ibu Waidah Sentana.</p>
+          </div>
+          <div class="wedding_information_rum">Dan di antara tanda-tanda (kebesaran)-Nya ialah Dia menciptakan pasangan-pasangan untukmu dari jenismu sendiri, agar kamu cenderung dan merasa tenteram kepadanya, dan Dia menjadikan di antaramu rasa kasih dan sayang. Sungguh, pada yang demikian itu benar-benar terdapat tanda-tanda (kebesaran Allah) bagi kaum yang berpikir.</div>
+          <div class="wedding_information_footer">(QS. Ar-Ruum : 21)</div>
+        </div>
+      </div>
+      <div class="wedding_event">
+        <div class="wrapper">
+          <div class="wedding_event_flower">&#10048;</div>
+          <div class="wedding_event_header">Undangan dan Acara</div>
+          <div class="wedding_event_venue">Acara akan kami selenggarakan di Gelanggang Remaja Kecamatan Matraman, Jl. Balai Rakyat, RT.8/RW.10, Utan Kayu Utara, Kec. Matraman, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta 13120.</div>
+          <div class="wedding_event_detail">
+            <div class="wedding_event_covenant">
+              <p class="wedding_event_covenant_header">Akad Pernikahan</p>
+              <div class="wedding_event_covenant_datetime">
+                <p class="wedding_event_covenant_datetime_date">Sabtu, 23 September 2023</p>
+                <p class="wedding_event_covenant_datetime_time">08:30 - 10:00 WIB</p>
+              </div>
+              <div class="wedding_event_covenant_vanue">Gelanggan Remaja Kecamatan Matraman</div>
+            </div>
+            <div class="wedding_event_reception">
+              <p class="wedding_event_reception_header">Resepsi Pernikahan</p>
+              <div class="wedding_event_reception_datetime">
+                <p class="wedding_event_reception_datetime_date">Sabtu, 23 September 2023</p>
+                <p class="wedding_event_reception_datetime_time">11:00 - 13:00 WIB</p>
+              </div>
+              <div class="wedding_event_reception_vanue">Gelanggan Remaja Kecamatan Matraman</div>
+            </div>
+          </div>
+          <div class="wedding_event_invitated">Turut Mengundang</div>
+          <p>Keluarga Besar Tajudin Nazir</p>
+          <p>Keluarga Besar M. Rahmat</p>
+          <p>Keluarga Besar Sidi Tando</p>
+          <p>Keluarga Besar Sentana</p>
+          <div class="wedding_event_note">Bahagia rasanya apabila anda berkenan hadir dan memberikan doa restu kepada kami.</div>
+        </div>
+      </div>
+      <div class="location">
+        <div class="wrapper">
+          <div class="location_flower">&#10048;</div>
+          <p class="location_header">Peta Lokasi</p>
+          <p class="location_link">Untuk dapat menuju ke lokasi acara, anda dapat membuka tautan link dibawah ini yang akan menampilkan peta berupa Google Maps</p>
+          <div class="location_icon">&#10083;</div>
+          <a href="https://www.google.com/maps/place/RV29%2BRPF+Gelanggang+Remaja+Kecamatan+Matraman,+Jl.+Balai+Rakyat,+RT.8%2FRW.10,+Utan+Kayu+Utara,+Kec.+Matraman,+Kota+Jakarta+Timur,+Daerah+Khusus+Ibukota+Jakarta+13120/@-6.1979343,106.869303,17z/data=!4m6!3m5!1s0x2e69f48b0a8181b1:0x3734c56f3ab9e7b6!8m2!3d-6.1979343!4d106.869303!16s%2Fg%2F1ptwh8f5k?gl=id">
+            <button class="location_button">Buka Link</button>
+          </a>
+          <img class="location_barcode_image" src="./assets/maps.png" alt="gift_box">
+        </div>
+      </div>
+      <div class="wedding_gift">
+        <div class="wrapper">
+          <div class="wedding_gift_flower">&#10048;</div>
+          <div class="wedding_gift_header">Kado Pernikahan</div>
+          <p class="wedding_gift_note">Jika berkenan untuk mengirimkan kado kepada pengantin bisa dilakukan dengan cara menghubungi kontak pribadi atau lewat rekening pengantin pria yang kami sediakan dibawah ini.</p>
+          <img class="wedding_gift_box" src="./assets/gift-box.png" alt="gift-box">
+          <p class="wedding_gift_account">BCA - Hanifsyah Solehuddin Nazir</p>
+          <p class="wedding_gift_account_number">123-456-789-101112</p>
+          <p class="wedding_gift_footer">Kami berdua mengucapkan terimakasih dan kami juga sangat mengharapkan atas kehadirannya di acara pernikahan kami berdua.</p>
+          <p class="wedding_gift_footer">Mohon maaf jika ada kekurangan dan kesalahan terutama dalam penulisan nama pada undangan.</p>
+          <p class="wedding_gift_thx">Terimakasih</p>
+          <p>Hanif & Intan</p>
+        </div>
+      </div>
+      <div class="footer">
+        <p class="x">This Digital Invitation is Developed by The Groom</p>
+        <p class="y">Powered by Vue.js</p>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.container {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  border: none;
+  overflow-x: hidden;
+}
+.welcome_section_detail {
+  color: black;
+  flex-direction: column;
+  display: flex;
+  align-items: center;
+  line-height: 40px;
+  width: 100%;
+  height: 850px;
+  background-image: url("./assets/pexels-dmitry-zvolskiy-1676123.jpg");
+  background-size: cover;
+  background-position: center;
+}
+.welcome_section_flower {
+  font-size: 40px;
+  color: floralwhite;
+  margin-top: 240px;
+}
+.welcome_section_bismillah {
+  font-size: 30px;
+  color: floralwhite;
+}
+.welcome_section_note {
+  font-size: 48px;
+  color: floralwhite;
+  font-weight: bold;
+  padding-top: 50px;
+}
+.welcome_section_cppcpw {
+  color: floralwhite;
+  font-size: 60px;
+  padding-top: 20px;
+}
+.welcome_section_date {
+  color: floralwhite;
+  font-size: 30px;
+  padding-top: 60px;
+}
+.welcome_section_love {
+  filter: brightness(95%);
+  font-size: 72px;
+  padding-top: 25px;
+  color: pink;
+}
+.wedding_information {
+  width: 100%;
+  height: 850px;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  background-image: url("./assets/IMG-2737 (3).jpg");
+  background-size: cover;
+  background-position: center;
+}
+.wrapper {
+  margin-top: auto;
+  margin-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.wedding_information_image {
+  width: 100%;
+  height: 100vh;
+  opacity: 10%;
+}
+.wedding_information_flower {
+  font-size: 36px;
+  color: palevioletred;
+}
+.wedding_information_salam {
+  font-size: 36px;
+  font-weight: bold;
+  font-style: italic;
+  line-height: 25px;
+}
+.wedding_information_detail {
+  font-family: sans-serif;
+  font-size: 18px;
+  text-align: center;
+  padding-top: 20px;
+  font-weight: normal;
+  line-height: 30px;
+  width: 80%;
+}
+.wedding_information_bride {
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  padding-top: 10px;
+}
+.wedding_information_cpp {
+  width: 90%;
+  text-align: center;
+  font-style: italic;
+}
+.wedding_information_cpw {
+  width: 90%;
+  text-align: center;
+  font-style: italic;
+}
+.wedding_information_cpp_name {
+  font-size: 30px;
+  padding-top: 15px;
+}
+.wedding_information_cpw_name {
+  font-size: 30px;
+  padding-top: 20px;
+}
+.wedding_information_love {
+  color: palevioletred;
+  width: 0px;
+  position: static;
+  top: 200px;
+  //margin-right: 20px;
+  background-color: black;
+}
+.wedding_information_rum {
+  width: 80%;
+  margin-top: 25px;
+  text-align: center;
+}
+.wedding_information_footer {
+  margin-top: 18px;
+}
+.wedding_event {
+  width: 100%;
+  height: 850px;
+  background-color: snow;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  background-image: url("./assets/pngwing.com (6).png");
+  background-size: cover;
+}
+.wedding_event_flower {
+  font-size: 36px;
+  color: palevioletred;
+  padding-top: 40px;
+}
+.wedding_event_header {
+  font-size: 36px;
+  font-weight: bold;
+  font-style: italic;
+  line-height: 25px;
+}
+.wedding_event_venue {
+  font-family: sans-serif;
+  width: 90%;
+  text-align: center;
+  line-height: 25px;
+  padding-top: 20px;
+}
+.wedding_event_detail {
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  padding-top: 10px;
+}
+.wedding_event_covenant {
+  width: 180px;
+  margin-left: auto;
+  margin-right: 10px;
+  height: 300px;
+  border: solid;
+  border-radius: 20px;
+  text-align: center;
+  font-style: italic;
+}
+.wedding_event_covenant_header {
+  padding-top: 20px;
+  padding-bottom: 20px;
+  font-size: 30px;
+  font-style: italic;
+  border-bottom: solid;
+}
+.wedding_event_covenant_datetime {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding-top: 15px;
+}
+.wedding_event_covenant_datetime_date {
+  font-size: 16px;
+  width: 105px;
+  margin-right: 10px;
+}
+.wedding_event_covenant_datetime_time {
+  font-size: 16px;
+  border-left: solid;
+}
+.wedding_event_covenant_vanue {
+  font-size: 16px;
+  padding-top: 5px;
+}
+.wedding_event_reception {
+  width: 180px;
+  margin-left: auto;
+  margin-right: auto;
+  height: 300px;
+  text-align: center;
+  border: solid;
+  border-radius: 20px;
+  font-style: italic;
+}
+.wedding_event_reception_header {
+  padding-top: 20px;
+  padding-bottom: 20px;
+  font-size: 30px;
+  font-style: italic;
+  border-bottom: solid;
+  padding-right: 5px;
+}
+.wedding_event_reception_datetime {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding-top: 15px;
+}
+.wedding_event_reception_datetime_date {
+  font-size: 16px;
+  width: 90px;
+  margin-right: 20px;
+}
+.wedding_event_reception_datetime_time {
+  font-size: 16px;
+  border-left: solid;
+}
+.wedding_event_reception_vanue {
+  font-size: 16px;
+  padding-top: 5px;
+}
+.wedding_event_invitated {
+  font-size: 30px;
+  font-style: italic;
+  font-weight: bold;
+  padding-top: 7px;
+}
+.wedding_event_note {
+  font-family: sans-serif;
+  width: 90%;
+  text-align: center;
+  line-height: 25px;
+  padding-top: 10px;
+}
+.wedding_event_date {
+  width: 10px;
+  padding-top: 300px;
+  padding-right: 10px;
+  font-size: 18px;
+  color: mediumvioletred;
+  font-weight: bold;
+  font-style: italic;
+}
+.location {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 850px;
+  background-image: url("./assets/pngwing.com (2).png");
+  background-repeat: no-repeat;
+  background-size: 40%;
+}
+.location_flower {
+  font-size: 36px;
+  padding-top: 30px;
+  color: palevioletred;
+}
+.location_header {
+  font-weight: bold;
+  font-style: italic;
+  font-size: 36px;
+}
+.location_link {
+  font-family: sans-serif;
+  font-size: 18px;
+  padding-top: 20px;
+  line-height: 25px;
+  text-align: center;
+  width: 90%;
+}
+.location_icon {
+  color: mediumvioletred;
+  font-size: 60px;
+}
+.location_button {
+  background-color: palevioletred;
+  color: white;
+  width: 150px;
+  height: 75px;
+  border-radius: 10px;
+  margin-top: -20px;
+  font-size: 24px;
+}
+.location_barcode {
+  font-family: sans-serif;
+  font-size: 18px;
+  padding-top: 50px;
+  text-align: center;
+}
+.location_barcode_image {
+  margin-top: 50px;
+  width: 350px;
+  height: 250px;
+  border: solid 1px;
+}
+.wedding_gift {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 850px;
+  background-color: snow;
+  background-image: url("./assets/pngwing.com (5).png");
+  background-repeat: no-repeat;
+  background-size: 100px;
+  background-position: left;
+  background-position-y: 750px;
+}
+.wedding_gift_flower {
+  font-size: 36px;
+  color: palevioletred;
+  padding-top: 40px;
+}
+.wedding_gift_header {
+  font-size: 36px;
+  font-style: italic;
+  font-weight: bold;
+}
+.wedding_gift_note {
+  font-family: sans-serif;
+  width: 90%;
+  text-align: center;
+  line-height: 25px;
+  padding-top: 20px;
+}
+.wedding_gift_box {
+  width: 100px;
+  height: 100px;
+  padding-top: 20px;
+}
+.wedding_gift_account {
+  font-size: 24px;
+  padding-top: 20px;
+}
+.wedding_gift_account_number {
+  font-size: 19px;
+}
+.wedding_gift_footer {
+  font-family: sans-serif;
+  width: 90%;
+  text-align: center;
+  line-height: 25px;
+  padding-top: 20px;
+}
+.wedding_gift_thx {
+  font-size: 30px;
+  padding-top: 30px;
+}
+.footer {
+  background-color: darkcyan;
+  width: 100%;
+  color: white;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+}
+.x {
+  font-family: sans-serif;
+  font-size: 8px;
+  margin-top: 5px;
+}
+.y {
+  font-size: 8px;
+  font-family: sans-serif;
+  margin-bottom: 5px;
+}
+</style>
